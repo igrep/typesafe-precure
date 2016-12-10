88057f814522b92e0421645e64341e3bcd971231
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-12-10 22:45:32 +0900

    Add Cure Echo

---
+++ src/ACME/PreCure/Textbook/Movie.hs
@@ -0,0 +1,6 @@
+module ACME.PreCure.Textbook.Movie
+  ( module M
+  ) where
+
+
+import           ACME.PreCure.Textbook.Movie.Types as M
---
+++ src/ACME/PreCure/Textbook/Movie/Words.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
+module ACME.PreCure.Textbook.Movie.Words where
+
+girlName_Ayumi = "坂上あゆみ"
+
+cureName_Echo = "キュアエコー"
+introducesHerselfAs_Echo = "思いよ届け！キュアエコー！"
+
+transformationSpeech_Echo =
+  ["みんなの思いを守るために", "心をひとつに！", introducesHerselfAs_Echo]
+
+
+purificationSpeech_Echo =
+  ["世界に響け、みんなの思い！", "プリキュア・ハートフルエコー！"]
