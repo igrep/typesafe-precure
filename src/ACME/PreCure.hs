c7efedbe55fe125bf9b912a0e8a72cdffafd6d40
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-12-11 14:41:47 +0900

    hide Monad.Skeleton when using on GHCi

--- src/ACME/PreCure.hs
+++ src/ACME/PreCure.hs
@@ -1,8 +1,14 @@
 module ACME.PreCure
   ( module P
+  , PreCureMonad
   ) where
 
 
-import           ACME.PreCure.Monad as P
+import qualified ACME.PreCure.Monad as M
+import           ACME.PreCure.Monad as P hiding (PreCureMonad)
 import           ACME.PreCure.Types as P
 import           ACME.PreCure.Textbook as P
+
+
+-- To hide Monad.Skeleton when using on GHCi
+type PreCureMonad = M.PreCureMonad
