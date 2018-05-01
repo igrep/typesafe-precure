dec40b20aa85c893ae2b9bec38638556b416b4cc
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-05-01 22:18:58 +0900

    Add HUGtto PreCure's new purifications

--- src/ACME/PreCure/Textbook/Hugtto/Profiles.hs
+++ src/ACME/PreCure/Textbook/Hugtto/Profiles.hs
@@ -33,9 +33,14 @@ transformedGroupsHugtto =
 specialItemsHugtto :: [SpecialItem]
 specialItemsHugtto =
   [ mkSpecialItem "Pre Heart" "プリハート" ["Mirai Crystal"]
-  , mkSpecialItem "Mirai Crystal Pink"   "ミライクリスタル・ピンク"   []
-  , mkSpecialItem "Mirai Crystal Blue"   "ミライクリスタル・ブルー"   []
-  , mkSpecialItem "Mirai Crystal Yellow" "ミライクリスタル・イエロー" []
+  , mkSpecialItem "Mirai Crystal Pink"   "ミライクリスタル・ピンク"    []
+  , mkSpecialItem "Mirai Crystal Blue"   "ミライクリスタル・ブルー"    []
+  , mkSpecialItem "Mirai Crystal Yellow" "ミライクリスタル・イエロー"  []
+  , mkSpecialItem "Melody Sword" "メロディソード" ["Mirai Crystal"]
+  , mkSpecialItem "Mirai Crystal Rose"   "ミライクリスタル・ローズ"    []
+  , mkSpecialItem "Mirai Crystal Navy"   "ミライクリスタル・ネイビー"  []
+  , mkSpecialItem "Mirai Crystal Orange" "ミライクリスタル・オレンジ"  []
+  , mkSpecialItem "Mirai Crystal White"  "ミライクリスタル・ホワイト"  []
   ]
 
 
@@ -69,6 +74,22 @@ purificationsHugtto =
   [ mkPurification ["CureYell"]   [mkIA "PreHeart" ["MiraiCrystalPink"]]   ["フレフレ！", "ハート・フォー・ユー！"]
   , mkPurification ["CureAnge"]   [mkIA "PreHeart" ["MiraiCrystalBlue"]]   ["フレフレ！", "ハート・フェザー！"]
   , mkPurification ["CureEtoile"] [mkIA "PreHeart" ["MiraiCrystalYellow"]] ["フレフレ！", "ハート・スター！"]
+  -- TODO: Aliases for Yell Tact, Ange Harp, Etoile Flute
+  -- https://www.youtube.com/watch?v=u3d_3aovyL8
+  , mkPurification ["CureYell"]   [mkIA "MelodySword" ["MiraiCrystalRose"]]   ["フラワーシュート!"]
+  , mkPurification ["CureAnge"]   [mkIA "MelodySword" ["MiraiCrystalNavy"]]   ["フェザーブラスト!"]
+  , mkPurification ["CureEtoile"] [mkIA "MelodySword" ["MiraiCrystalOrange"]] ["スタースラッシュ！"]
+  , mkPurification
+      ["CureYell", "CureAnge", "CureEtoile"]
+      [mkIA "MelodySword" ["MiraiCrystalRose"], mkIA "MelodySword" ["MiraiCrystalNavy"], mkIA "MelodySword" ["MiraiCrystalOrange"]]
+      [ "ミライクリスタル！"
+      , "エールタクト！"
+      , "アンジュハープ！"
+      , "エトワールフルート！"
+      , "心のトゲトゲ、飛んでいけ〜！"
+      , "プリキュア・トリニティコンサート！"
+      , "HUGっとプリキュア！エール・フォー・ユー！"
+      ]
   ]
 
 
