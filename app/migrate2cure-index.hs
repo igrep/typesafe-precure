85e06f9df794b84142683dbbd65c42c27368322d
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-08-12 18:01:40 +0900

    Ready to write all files

--- app/migrate2cure-index.hs
+++ app/migrate2cure-index.hs
@@ -3,19 +3,21 @@
 {-# LANGUAGE RecordWildCards   #-}
 
 import           Control.Applicative           (optional, (<|>))
-import           Control.Monad                 (mapM_, void, guard)
+import           Control.Monad                 (guard, mapM_, void)
 import qualified Data.Attoparsec.Text          as A
 import           Data.Char                     (isUpper)
 import           Data.List                     (break)
 import           Data.Maybe                    (isJust, mapMaybe)
-import           Data.Monoid                   ((<>))
+import           Data.Monoid                   (mconcat, (<>))
 import qualified Data.String.Here.Interpolated as HI
 import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as T
+import qualified Data.Text.Lazy                as TL
+import qualified Data.Text.Lazy.Builder        as TL
+import qualified Data.Text.Lazy.IO             as TL
 import qualified System.Directory              as D
 import qualified System.FilePath               as F
 import qualified Text.Parser.Substring         as PS
-import           Text.Pretty.Simple            (pPrint)
 
 import           ACME.PreCure.Index.Types
 
@@ -24,22 +26,26 @@ import           Debug.NoTrace
 
 main :: IO ()
 main = do
-  let targetRoot = "src/ACME/PreCure/Textbook/"
   targetDirNames <-
     filter (\path -> notYetMigrated path && isDirectory path)
     <$> D.listDirectory targetRoot
   genTypesHs targetDirNames
   putStrLn "\n\n----------------------------------------------------\n"
-  genProfilesHs (map (targetRoot ++) targetDirNames)
+  genProfilesHs targetDirNames
 
 
-genTypesHs :: [FilePath] -> IO ()
+targetRoot :: FilePath
+targetRoot = "src/ACME/PreCure/Textbook/"
+
+
+genTypesHs :: [String] -> IO ()
 genTypesHs = mapM_ (T.putStr . typeHsFromSeriesName)
 
 genProfilesHs :: [FilePath] -> IO ()
-genProfilesHs = mapM_ $ \seriesRoot -> do
+genProfilesHs = mapM_ $ \seriesName -> do
+  let seriesRoot = targetRoot ++ seriesName
   putStrLn $ "\n-- " ++ seriesRoot ++ " --"
-  pPrint . classify . parseAll =<< T.readFile (seriesRoot ++ "/Types.hs")
+  TL.putStr . buildProfileHs seriesName . classify . parseAll =<< T.readFile (seriesRoot ++ "/Types.hs")
 
 
 parseAll :: T.Text -> [Aux]
@@ -74,30 +80,137 @@ typeHsFromSeriesName seriesName = T.unlines $ map (T.strip . T.pack) $ lines src
 
     import           ACME.PreCure.Textbook.${seriesName}.Profiles
 
-    {-# ANN module girls${seriesName} #-}
-    $(declareGirls girls${seriesName})
+    {-# ANN module girls #-}
+    $(declareGirls girls)
 
-    {-# ANN module transformees${seriesName} #-}
-    $(declareTransformees transformees${seriesName})
+    {-# ANN module transformees #-}
+    $(declareTransformees transformees)
 
-    {-# ANN module transformedGroups${seriesName} #-}
-    $(declareTransformedGroups transformedGroups${seriesName})
+    {-# ANN module transformedGroups #-}
+    $(declareTransformedGroups transformedGroups)
 
-    {-# ANN module specialItems${seriesName} #-}
-    $(declareSpecialItems specialItems${seriesName})
+    {-# ANN module specialItems #-}
+    $(declareSpecialItems specialItems)
 
-    {-# ANN module transformations${seriesName} #-}
-    $(declareTransformations transformations${seriesName})
+    {-# ANN module transformations #-}
+    $(declareTransformations transformations)
 
-    {-# ANN module purifications${seriesName} #-}
-    $(declarePurifications purifications${seriesName})
+    {-# ANN module purifications #-}
+    $(declarePurifications purifications)
 
-    {-# ANN module nonItemPurifications${seriesName} #-}
-    $(declarePurifications nonItemPurifications${seriesName})
+    {-# ANN module nonItemPurifications #-}
+    $(declarePurifications nonItemPurifications)
   |]
 
 
---------------------------------------------------- classifier
+--------------------------------------------------- classifier and builder
+
+
+buildProfileHs :: String -> Index -> TL.Text
+buildProfileHs seriesName Index {..} = TL.toLazyText $ mconcat
+  [ profilesHsHeader
+  , sourceOfGirls indexGirls
+  , sourceOfTransformees indexTransformees
+  , sourceOfTransformedGroups indexTransformedGroups
+  , sourceOfSpecialItems indexSpecialItems
+  , sourceOfTransformations indexTransformations
+  , sourceOfPurifications indexPurifications
+  , sourceOfNonItemPurifications indexNonItemPurifications
+  ]
+
+ where
+  profilesHsHeader = TL.fromLazyText $ TL.unlines
+    [ "{-# OPTIONS_GHC -fno-warn-missing-signatures #-}"
+    , "{-# LANGUAGE OverloadedStrings #-}"
+    , ""
+    , "module ACME.PreCure.Textbook.Hugtto.Profiles where"
+    , ""
+    , "import           ACME.PreCure.Index.Types"
+    , "import           ACME.PreCure.Textbook." <> TL.pack seriesName <> ".Words"
+    , ""
+    , ""
+    ]
+
+  sourceOfGirls girls = mconcat
+    ["girls :: [Girl]\n", "girls =\n", toMultiLineText (map toMkGirl girls), "\n", "\n"]
+
+  toMkGirl Girl {..} = "mkGirl " <> TL.pack girlNameEn <> " " <> TL.pack girlNameJa
+
+  sourceOfTransformees transformees = mconcat
+    ["transformees :: [Transformee]\n", "transformees =\n", toMultiLineText (map toMkTransformee transformees), "\n", "\n"]
+
+  toMkTransformee Transformee {..} = TL.unlines
+    [ "mkTransformee"
+    , "      " <> TL.pack transformedNameEn
+    , "      " <> TL.pack transformedVariationEn
+    , "      " <> TL.pack transformedNameJa
+    , "      " <> TL.pack transformedVariationJa
+    , "      " <> TL.pack transformedIntroducesHerselfAs
+    ]
+
+  sourceOfTransformedGroups transformedGroups = mconcat
+    ["transformedGroups :: [TransformedGroup]\n", "transformedGroups =\n", toMultiLineText (map toMkTransformedGroup transformedGroups), "\n", "\n"]
+
+  toMkTransformedGroup TransformedGroup {..} = TL.intercalate " "
+    [ "mkTransformedGroup"
+    , "[" <> TL.intercalate ", " (map (TL.pack . show) transformedGroupTransformerIds) <> "]"
+    , TL.pack transformedGroupNameEn
+    , TL.pack transformedGroupVariationEn
+    , TL.pack transformedGroupNameJa
+    , TL.pack transformedGroupVariationJa
+    ]
+
+  sourceOfSpecialItems specialItems = mconcat
+    ["specialItems :: [SpecialItem]\n", "specialItems =\n", toMultiLineText (map toMkSpecialItem specialItems), "\n", "\n"]
+
+  toMkSpecialItem SpecialItem {..} = TL.intercalate " "
+    [ "mkSpecialItem"
+    , TL.pack specialItemNameEn
+    , TL.pack specialItemNameJa
+    , "[" <> TL.intercalate ", " (map TL.pack specialItemAttachments) <> "]"
+    ]
+
+  sourceOfTransformations transformations = mconcat
+    ["transformations :: [Transformation]\n", "transformations =\n", toMultiLineText (map toMkTransformation transformations), "\n", "\n"]
+
+  toMkTransformation Transformation {..} = TL.unlines
+    [ "mkTransformation"
+    , "      [" <> TL.intercalate ", " (map toMkIa transformationTransformers) <> "]"
+    , "      [" <> TL.intercalate ", " (map toMkIa transformationSpecialItems) <> "]"
+    , "      "  <> TL.pack (show transformationTransformees)
+    , "      "  <> TL.pack (head transformationSpeech)
+    --                     ^ The collected transformationSpeech-es should have only one element
+    --                       to refer the variable name of transformationSpeech
+    ]
+
+  sourceOfPurifications purifications = mconcat
+    ["purifications :: [Purification]\n", "purifications =\n", toMultiLineText (map toMkPurification purifications), "\n", "\n"]
+
+  toMkPurification Purification {..} = TL.unlines
+    [ "mkPurification"
+    , "      [" <> TL.intercalate ", " (map toMkIa purificationPurifiers) <> "]"
+    , "      [" <> TL.intercalate ", " (map toMkIa purificationSpecialItems) <> "]"
+    , "      "  <> TL.pack (head purificationSpeech)
+    --                     ^ The collected purificationSpeech-es should have only one element
+    --                       to refer the variable name of purificationSpeech
+    ]
+
+  sourceOfNonItemPurifications nonItemPurifications = mconcat
+    ["nonItemPurifications :: [NonItemPurification]\n", "nonItemPurifications =\n", toMultiLineText (map toMkNonItemPurification nonItemPurifications), "\n", "\n"]
+
+  toMkNonItemPurification NonItemPurification {..} = TL.intercalate " "
+    ["mkNonItemPurification"
+    , "[" <> TL.intercalate ", " (map toMkIa nonItemPurificationPurifiers) <> "]"
+    , TL.pack (head nonItemPurificationSpeech)
+    --         ^ The collected nonItemPurificationSpeech-es should have only one element
+    --           to refer the variable name of nonItemPurificationSpeech
+    ]
+
+  toMultiLineText ts = TL.fromLazyText $ "  [ " <> TL.intercalate "\n  , " ts <> "\n  ]\n"
+
+  toMkIa (IdAttachments i []) = TL.pack $ show i
+  toMkIa (IdAttachments i as) = "mkIA " <> TL.pack (show i) <> " [" <> TL.intercalate ", " (map toMkIa as) <> "]"
+
 
 classify :: [Aux] -> Index
 classify auxs = index { indexSpecialItems = selectSpecialItems index specialItems }
