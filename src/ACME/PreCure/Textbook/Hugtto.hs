f46dc92e70e8b80dd92205a541c4b0454e26f485
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-08-16 21:23:08 +0900

    Rearrange module structure
    Export only type from Textbook module to hide the dirty identifiers in Profiles and Words modules
    
    Delete no more used Types modules

--- src/ACME/PreCure/Textbook/Hugtto.hs
+++ src/ACME/PreCure/Textbook/Hugtto.hs
@@ -1,2 +1,29 @@
-{-# OPTIONS_GHC -fno-warn-dodgy-exports -fno-warn-unused-imports #-}
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module ACME.PreCure.Textbook.Hugtto where
+
+import           ACME.PreCure.Types.TH
+
+import           ACME.PreCure.Textbook.Hugtto.Profiles
+
+{-# ANN module girlsHugtto #-}
+$(declareGirls girlsHugtto)
+
+{-# ANN module transformeesHugtto #-}
+$(declareTransformees transformeesHugtto)
+
+{-# ANN module transformedGroupsHugtto #-}
+$(declareTransformedGroups transformedGroupsHugtto)
+
+{-# ANN module specialItemsHugtto #-}
+$(declareSpecialItems specialItemsHugtto)
+
+{-# ANN module transformationsHugtto #-}
+$(declareTransformations transformationsHugtto)
+
+{-# ANN module purificationsHugtto #-}
+$(declarePurifications purificationsHugtto)
--- src/ACME/PreCure/Textbook/KirakiraALaMode.hs
+++ src/ACME/PreCure/Textbook/KirakiraALaMode.hs
@@ -1,2 +1,29 @@
-{-# OPTIONS_GHC -fno-warn-dodgy-exports -fno-warn-unused-imports #-}
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module ACME.PreCure.Textbook.KirakiraALaMode where
+
+import           ACME.PreCure.Types.TH
+
+import           ACME.PreCure.Textbook.KirakiraALaMode.Profiles
+
+{-# ANN module girlsKirakiraALaMode #-}
+$(declareGirls girlsKirakiraALaMode)
+
+{-# ANN module transformeesKirakiraALaMode #-}
+$(declareTransformees transformeesKirakiraALaMode)
+
+{-# ANN module transformedGroupsKirakiraALaMode #-}
+$(declareTransformedGroups transformedGroupsKirakiraALaMode)
+
+{-# ANN module specialItemsKirakiraALaMode #-}
+$(declareSpecialItems specialItemsKirakiraALaMode)
+
+{-# ANN module transformationsKirakiraALaMode #-}
+$(declareTransformations transformationsKirakiraALaMode)
+
+{-# ANN module purificationsKirakiraALaMode #-}
+$(declarePurifications purificationsKirakiraALaMode)
