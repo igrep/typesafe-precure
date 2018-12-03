68f1b97f90671d85c532d5561b26891de45ee085
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2018-12-03 09:07:53 +0900

    Implement polymorphic update for Record

--- src/ACME/PreCure/Monad/Super/Types.hs
+++ src/ACME/PreCure/Monad/Super/Types.hs
@@ -11,7 +11,17 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module ACME.PreCure.Monad.Super.Types where
+module ACME.PreCure.Monad.Super.Types
+  ( GirlS
+  , GirlOrPreCureS
+  , IsTransformedS
+  , StatusTable
+  , HasTransformed (..)
+  , AsGirl
+  , IsTransformedOrNot (..)
+  , PSet (PSetResult)
+  , pSet
+  ) where
 
 import           Data.Extensible
 import           Data.Extensible.HList
@@ -43,11 +53,6 @@ class IsTransformedOrNot g where
   isTransformed :: g -> HasTransformed b
 
 
-{-
-class HEq (x :: k) (y :: k) (b :: Bool) | x y -> b
-instance ((Proxy x == Proxy y) ~ b) => HEq x y b
--}
-
 class HSwapValIf (b :: Bool) (val1 :: Type) (kv :: Assoc k Type) where
   type HSwapValIfResult b val1 kv :: Assoc k Type
   hSwapValIf :: Proxy b -> val1 -> Field Identity kv -> Field Identity (HSwapValIfResult b val1 kv)
@@ -61,47 +66,25 @@ instance HSwapValIf 'False val1 (k ':> val2) where
   hSwapValIf _b _val1 kv = kv
 
 
-class HSet (key :: k) (val :: Type) (kvs :: [Assoc k Type]) where
-  type HSetResult key val kvs :: [Assoc k Type]
-  hSet :: Proxy key -> val -> HList (Field Identity) kvs -> HList (Field Identity) (HSetResult key val kvs)
+-- | Polymorphic update for 'HList'
+class PSet (key :: k) (val :: Type) (kvs :: [Assoc k Type]) where
+  type PSetResult key val kvs :: [Assoc k Type]
+  hPSet :: Proxy key -> val -> HList (Field Identity) kvs -> HList (Field Identity) (PSetResult key val kvs)
 
-instance HSet key val '[] where
-  type HSetResult key val '[] = '[]
-  hSet _key _val HNil = HNil
-
-instance
-  (HSwapValIf (key1 == key2) val (key2 ':> oldVal), HSet key1 val kvs)
-  => HSet key1 val ((key2 ':> oldVal) ': kvs)
- where
-  type HSetResult key1 val ((key2 ':> oldVal) ': kvs) =
-    HSwapValIfResult (key1 == key2) val (key2 ':> oldVal) ': HSetResult key1 val kvs
-  hSet key1 val (HCons kv kvs) = hSwapValIf (Proxy :: Proxy (key1 == key2)) val kv `HCons` hSet key1 val kvs
-
-
-{-
-class TransformGirl
-  (g :: GirlS)
-  (xs :: [Assoc GirlS IsTransformedS])
-  (ys :: [Assoc GirlS IsTransformedS])
-  | g xs -> ys
- where
-  transformGirl :: g -> Record xs -> Record ys
-
-instance TransformGirl g '[] where
-  type ToTransformed g xs = '[]
-  transformGirl _girl _xs = nil
+instance PSet key val '[] where
+  type PSetResult key val '[] = '[]
+  hPSet _key _val HNil = HNil
 
 instance
-  (EqTy g1 g2 b, TransformGirl g1 xs ys)
-  => TransformGirl
-    g1
-    (g2 :> t ': xs)
-    (g >: HasNotTransformed 'True ': TransformGirl g xs)
+  (HSwapValIf (key1 == key2) val (key2 ':> oldVal), PSet key1 val kvs)
+  => PSet key1 val ((key2 ':> oldVal) ': kvs)
  where
-  transformGirl girl xs =
-    itemAssoc (Proxy :: Proxy g) @= HasTransformed <: transformGirl girl left
+  type PSetResult key1 val ((key2 ':> oldVal) ': kvs) =
+    HSwapValIfResult (key1 == key2) val (key2 ':> oldVal) ': PSetResult key1 val kvs
+  hPSet key1 val (HCons kv kvs) = hSwapValIf (Proxy :: Proxy (key1 == key2)) val kv `HCons` hPSet key1 val kvs
 
-instance TransformGirl g (g' >: t ': xs) where
-  type ToTransformed g xs = g' >: t ': TransformGirl g left
-  transformGirl girl xs = x <: transformGirl girl left
--}
+
+-- | Polymorphic update for 'Record'
+--   TODO: Type error when key doesn't exist in kvs
+pSet :: forall key val kvs. PSet key val kvs => Proxy key -> val -> Record kvs -> Record (PSetResult key val kvs)
+pSet _key val = fromHList . hPSet (Proxy :: Proxy key) val . toHList
