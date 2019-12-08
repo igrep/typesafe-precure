f0727fa6f4e224471e0957a8842819ff56a6e066
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-12-08 21:47:34 +0900

    Fix hlint's warnings

--- test/ACME/PreCureSpec.hs
+++ test/ACME/PreCureSpec.hs
@@ -19,36 +19,36 @@ spec = do
   let specialForm = (CureMiracle_OverTheRainbow, CureMagical_OverTheRainbow, CureFelice_OverTheRainbow)
 
   describe "transformedStyle" $ do
-    it "returns transformed precure" $ do
-      (transformedStyle (Mirai, Liko) (Mofurun LinkleStoneDia)) `shouldBe` (CureMiracle, CureMagical)
+    it "returns transformed precure" $
+      transformedStyle (Mirai, Liko) (Mofurun LinkleStoneDia) `shouldBe` (CureMiracle, CureMagical)
 
-    it "some precures can transform into special form" $ do
-      (transformedStyle (CureMiracle, CureMagical, CureFelice) (Mofurun LinkleStoneAlexandrite, LinkleSmarthon LinkleStoneAlexandrite)) `shouldBe` specialForm
+    it "some precures can transform into special form" $
+      transformedStyle (CureMiracle, CureMagical, CureFelice) (Mofurun LinkleStoneAlexandrite, LinkleSmarthon LinkleStoneAlexandrite) `shouldBe` specialForm
 
   describe "transformationSpeech" $ do
-    it "returns group transformation speech of the pair" $ do
-      (transformationSpeech (Mirai, Liko) (Mofurun LinkleStoneDia)) `shouldBe` ["キュアップ・ラパパ！　ダイヤ！", "ミラクル・マジカル・ジュエリーレ！", "ふたりの奇跡！キュアミラクル！", "ふたりの魔法！キュアマジカル！", "魔法つかいプリキュア！！"]
+    it "returns group transformation speech of the pair" $
+      transformationSpeech (Mirai, Liko) (Mofurun LinkleStoneDia) `shouldBe` ["キュアップ・ラパパ！　ダイヤ！", "ミラクル・マジカル・ジュエリーレ！", "ふたりの奇跡！キュアミラクル！", "ふたりの魔法！キュアマジカル！", "魔法つかいプリキュア！！"]
 
-    it "returns group transformation speech with alexandrite" $ do
-      (transformationSpeech (CureMiracle, CureMagical, CureFelice) (Mofurun LinkleStoneAlexandrite, LinkleSmarthon LinkleStoneAlexandrite)) `shouldBe` ["キュアップ・ラパパ！　アレキサンドライト！", "魔法つかいプリキュア！　オーバー・ザ・レインボー！"]
+    it "returns group transformation speech with alexandrite" $
+      transformationSpeech (CureMiracle, CureMagical, CureFelice) (Mofurun LinkleStoneAlexandrite, LinkleSmarthon LinkleStoneAlexandrite) `shouldBe` ["キュアップ・ラパパ！　アレキサンドライト！", "魔法つかいプリキュア！　オーバー・ザ・レインボー！"]
 
-  describe "purificationSpeech" $ do
-    it "returns purification speech" $ do
-      (purificationSpeech (CureMiracle, CureMagical) (Mofurun LinkleStoneDia, LinkleStick LinkleStoneDia)) `shouldBe` ["ダイヤ！永遠の輝きを私達の手に！", "フルフルリンクル！", "プリキュア！ダイヤモンド・エターナル！"]
+  describe "purificationSpeech" $
+    it "returns purification speech" $
+      purificationSpeech (CureMiracle, CureMagical) (Mofurun LinkleStoneDia, LinkleStick LinkleStoneDia) `shouldBe` ["ダイヤ！永遠の輝きを私達の手に！", "フルフルリンクル！", "プリキュア！ダイヤモンド・エターナル！"]
 
   describe "TransformedGroup" $ do
     describe "CureMagical, CureMiracle, and CureFelice" $ do
-      it "are Maho Girls PreCure" $ do
+      it "are Maho Girls PreCure" $
         groupName (CureMiracle, CureMagical, CureFelice) `shouldBe` "魔法つかいプリキュア！"
 
-      it "Their variation is dia style" $ do
+      it "Their variation is dia style" $
         groupVariation (CureMiracle, CureMagical, CureFelice) `shouldBe` "ダイヤスタイル"
 
     describe "CureMagical, CureMiracle, and CureFelice (special form)" $ do
-      it "are Maho Girls PreCure" $ do
+      it "are Maho Girls PreCure" $
         groupName specialForm `shouldBe` "魔法つかいプリキュア！"
 
-      it "Their variation is over the raibow style" $ do
+      it "Their variation is over the raibow style" $
         groupVariation specialForm `shouldBe` "オーバー・ザ・レインボー"
 
   let action = do
@@ -56,7 +56,7 @@ spec = do
         purifyWithoutItem (CureBlack, CureWhite)
         purify (CureBlack, CureWhite) RainbowBracelets
 
-  describe "composeEpisode" $ do
+  describe "composeEpisode" $
     it "returns accumulated lines" $ do
       let actual = composeEpisode action
           expected =
@@ -65,6 +65,6 @@ spec = do
                 ++ purificationSpeech (CureBlack, CureWhite) RainbowBracelets
       actual `shouldBe` expected
 
-  describe "printEpisodeWith" $ do
-    it "prints lines by line" $ do
+  describe "printEpisodeWith" $
+    it "prints lines by line" $
       printEpisodeWith (defaultEpisodeConfig { lineIntervalMicroSec = 300 * 1000 }) action
