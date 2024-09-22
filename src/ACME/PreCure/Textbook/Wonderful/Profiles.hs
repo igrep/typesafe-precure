ca936c7c9a4b92a7b0972b2f64d39493b6a5aa9a
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2024-09-22 19:00:52 +0900

    Add more transformations and purifications from "Wonderful Pretty Cure!"

--- src/ACME/PreCure/Textbook/Wonderful/Profiles.hs
+++ src/ACME/PreCure/Textbook/Wonderful/Profiles.hs
@@ -3,7 +3,7 @@
 
 module ACME.PreCure.Textbook.Wonderful.Profiles where
 
--- import           Data.String              (IsString, fromString)
+import           Data.String              (IsString)
 
 import           ACME.PreCure.Index.Types
 
@@ -41,12 +41,39 @@ transformees =
       "キュアリリアン"
       ""
       introducesHerselfAs_Lillian
+
+  , mkTransformee
+      "Cure Wonderful"
+      "Diamond Ribbon Style"
+      "キュアワンダフル"
+      "ダイアモンドリボンスタイル"
+      introducesHerselfAs_Wonderful
+  , mkTransformee
+      "Cure Friendy"
+      "Diamond Ribbon Style"
+      "キュアフレンディ"
+      "ダイアモンドリボンスタイル"
+      introducesHerselfAs_Friendy
+  , mkTransformee
+      "Cure Nyammy"
+      "Diamond Ribbon Style"
+      "キュアニャミー"
+      "ダイアモンドリボンスタイル"
+      introducesHerselfAs_Nyammy
+  , mkTransformee
+      "Cure Lillian"
+      "Diamond Ribbon Style"
+      "キュアリリアン"
+      "ダイアモンドリボンスタイル"
+      introducesHerselfAs_Lillian
   ]
 
 transformedGroups :: [TransformedGroup]
 transformedGroups =
   [ mkTransformedGroup ["CureWonderful", "CureFriendy"] neW "" groupNameWonderful ""
   , mkTransformedGroup ["CureNyammy", "CureLillian"] neN "" groupNameNyanderful ""
+  , mkTransformedGroup groupMembers neW "" groupNameWonderful ""
+  , mkTransformedGroup groupMembers_diamondRibbon neN "Diamond Ribbon Style" groupNameWonderful "ダイアモンドリボンスタイル"
   ]
  where
   neW = "Wonderful! Pretty Cure"
@@ -68,74 +95,105 @@ specialItems =
   , mkSpecialItem "Kirarin Hamster" "キラリンハムスター"
   , mkSpecialItem "Kirarin Fox" "キラリンキツネ"
   , mkSpecialItem "Kirarin Panda" "キラリンパンダ"
+  , mkSpecialItem "Kirarin Swan" "キラリンスワン"
+
+  , mkSpecialItem "Niko" "ニコ"
+  , mkSpecialItem "Diamond Ribbon Castle" "ダイヤモンドリボンキャッスル"
+  , mkSpecialItem "Dia Heart Key" "ダイヤハートキー"
   ]
 
 transformations :: [Transformation]
 transformations =
   [ mkTransformation
-      ["Komugi"]
-      ["WonderfulPact"]
-      ["CureWonderful"]
-      [ wonderfulPact
-      , preCureMyEvolution
-      , threeTwoOne
-      , introducesHerselfAs_Wonderful
-      ]
+    ["Komugi"]
+    ["WonderfulPact"]
+    ["CureWonderful"]
+    [ wonderfulPact
+    , preCureMyEvolution
+    , threeTwoOne
+    , introducesHerselfAs_Wonderful
+    ]
   , mkTransformation
-      ["Iroha"]
-      ["WonderfulPact"]
-      ["CureFriendy"]
-      [ wonderfulPact
-      , preCureMyEvolution
-      , threeTwoOne
-      , introducesHerselfAs_Friendy
-      ]
+    ["Iroha"]
+    ["WonderfulPact"]
+    ["CureFriendy"]
+    [ wonderfulPact
+    , preCureMyEvolution
+    , threeTwoOne
+    , introducesHerselfAs_Friendy
+    ]
   , mkTransformation
-      ["Yuki"]
-      ["ShinyCatsPact"]
-      ["CureNyammy"]
-      [ shinyCatsPact
-      , preCureMyEvolution
-      , hoppeNiKirameki
-      , ripWaCuteNi
-      , introducesHerselfAs_Nyammy
-      ]
+    ["Yuki"]
+    ["ShinyCatsPact"]
+    ["CureNyammy"]
+    [ shinyCatsPact
+    , preCureMyEvolution
+    , hoppeNiKirameki
+    , ripWaCuteNi
+    , introducesHerselfAs_Nyammy
+    ]
   , mkTransformation
-      ["Mayu"]
-      ["ShinyCatsPact"]
-      ["CureLillian"]
-      [ shinyCatsPact
-      , preCureMyEvolution
-      , memotoNiKirameki
-      , ripWaCuteNi
-      , introducesHerselfAs_Lillian
-      ]
+    ["Mayu"]
+    ["ShinyCatsPact"]
+    ["CureLillian"]
+    [ shinyCatsPact
+    , preCureMyEvolution
+    , memotoNiKirameki
+    , ripWaCuteNi
+    , introducesHerselfAs_Lillian
+    ]
 
   , mkTransformation
-      ["Komugi", "Iroha"]
-      ["WonderfulPact", "WonderfulPact"]
-      ["CureWonderful", "CureFriendy"]
-      [ wonderfulPact
-      , preCureMyEvolution
-      , threeTwoOne
-      , introducesHerselfAs_Wonderful
-      , introducesHerselfAs_Friendy
-      , "せーのっ！"
-      , groupNameWonderful
-      ]
+    ["Komugi", "Iroha"]
+    ["WonderfulPact", "WonderfulPact"]
+    ["CureWonderful", "CureFriendy"]
+    [ wonderfulPact
+    , preCureMyEvolution
+    , threeTwoOne
+    , introducesHerselfAs_Wonderful
+    , introducesHerselfAs_Friendy
+    , "せーのっ！"
+    , groupNameWonderful
+    ]
 
   , mkTransformation
-      ["Yuki", "Mayu"]
-      ["ShinyCatsPact", "ShinyCatsPact"]
-      ["CureLillian", "CureNyammy"]
-      [ shinyCatsPact
-      , preCureMyEvolution
-      , memotoNiKirameki
-      , ripWaCuteNi
-      , introducesHerselfAs_Nyammy
-      , introducesHerselfAs_Lillian
-      , groupNameNyanderful
-      ]
+    ["Yuki", "Mayu"]
+    ["ShinyCatsPact", "ShinyCatsPact"]
+    ["CureLillian", "CureNyammy"]
+    [ shinyCatsPact
+    , preCureMyEvolution
+    , memotoNiKirameki
+    , ripWaCuteNi
+    , introducesHerselfAs_Nyammy
+    , introducesHerselfAs_Lillian
+    , groupNameNyanderful
+    ]
+
+  , mkTransformation
+    ["Komugi", "Iroha", "Yuki", "Mayu"]
+    ["WonderfulPact", "WonderfulPact", "ShinyCatsPact", "ShinyCatsPact"]
+    groupMembers
+    [ wonderfulPact
+    , shinyCatsPact
+    , preCureMyEvolution
+    , threeTwoOne
+    , introducesHerselfAs_Wonderful
+    , introducesHerselfAs_Friendy
+    , introducesHerselfAs_Nyammy
+    , introducesHerselfAs_Lillian
+    , "みんな一緒に！"
+    , "せーのっ！"
+    , groupNameWonderful
+    ]
+
+  , mkTransformation
+    groupMembers
+    ["Niko", mkIA "DiamondRibbonCastle" ["DiaHeartKey"]]
+    groupMembers_diamondRibbon
+    [ "ニコの力をみんなに！"
+    , "ひらけ！ニコエボリューション！！"
+    , "ダイヤモンドリボンスタイル！"
+    ]
   ]
  where
   wonderfulPact :: String
@@ -160,106 +218,239 @@ transformations =
   ripWaCuteNi = "リップはキュートに！"
 
 
+groupMembers :: IsString s => [s]
+groupMembers = ["CureWonderful", "CureFriendy", "CureNyammy", "CureLillian"]
+
+groupMembers_diamondRibbon :: IsString s => [s]
+groupMembers_diamondRibbon =
+  [ "CureWonderful_DiamondRibbonStyle"
+  , "CureFriendy_DiamondRibbonStyle"
+  , "CureNyammy_DiamondRibbonStyle"
+  , "CureLillian_DiamondRibbonStyle"
+  ]
+
+
 purifications :: [Purification]
 purifications =
   [ mkPurification
-      ["CureWonderful"]
-      ["FriendlyTact", "KirarinPenguin"]
-      [ friendlyTact
-      , wanWanWonderful
-      , helpKirarinAnimal
-      , "ペンギン！"
-      ]
+    ["CureWonderful"]
+    ["FriendlyTact", "KirarinPenguin"]
+    [ friendlyTact
+    , wanWanWonderful
+    , helpKirarinAnimal
+    , "ペンギン！"
+    ]
   , mkPurification
-      ["CureWonderful"]
-      ["FriendlyTact", "KirarinLion"]
-      [ friendlyTact
-      , wanWanWonderful
-      , helpKirarinAnimal
-      , "ライオン！"
-      ]
+    ["CureWonderful"]
+    ["FriendlyTact", "KirarinLion"]
+    [ friendlyTact
+    , wanWanWonderful
+    , helpKirarinAnimal
+    , "ライオン！"
+    ]
   , mkPurification
-      ["CureWonderful"]
-      ["FriendlyTact", "KirarinBear"]
-      [ friendlyTact
-      , wanWanWonderful
-      , helpKirarinAnimal
-      , "ベアー！"
-      ]
+    ["CureWonderful"]
+    ["FriendlyTact", "KirarinBear"]
+    [ friendlyTact
+    , wanWanWonderful
+    , helpKirarinAnimal
+    , "ベアー！"
+    ]
+  , mkPurification
+    ["CureWonderful"]
+    ["FriendlyTact", "KirarinFawn"]
+    [ friendlyTact
+    , wanWanWonderful
+    , helpKirarinAnimal
+    , "コジカ！"
+    ]
+  , mkPurification
+    ["CureWonderful"]
+    ["FriendlyTact", "KirarinSwan"]
+    [ friendlyTact
+    , wanWanWonderful
+    , helpKirarinAnimal
+    , "スワン！"
+    ]
 
   , mkPurification
-      ["CureFriendy"]
-      ["FriendlyTact", "KirarinRabbit"]
-      [ friendlyTact
-      , fureFureFriendy
-      , helpKirarinAnimal
-      , "ウサギ！"
-      ]
+    ["CureFriendy"]
+    ["FriendlyTact", "KirarinRabbit"]
+    [ friendlyTact
+    , fureFureFriendy
+    , helpKirarinAnimal
+    , "ウサギ！"
+    ]
   , mkPurification
-      ["CureFriendy"]
-      ["FriendlyTact", "KirarinPenguin"]
-      [ friendlyTact
-      , fureFureFriendy
-      , helpKirarinAnimal
-      , "ペンギン！"
-      ]
+    ["CureFriendy"]
+    ["FriendlyTact", "KirarinPenguin"]
+    [ friendlyTact
+    , fureFureFriendy
+    , helpKirarinAnimal
+    , "ペンギン！"
+    ]
   , mkPurification
-      ["CureFriendy"]
-      ["FriendlyTact", "KirarinLion"]
-      [ friendlyTact
-      , fureFureFriendy
-      , helpKirarinAnimal
-      , "ライオン！"
-      ]
+    ["CureFriendy"]
+    ["FriendlyTact", "KirarinLion"]
+    [ friendlyTact
+    , fureFureFriendy
+    , helpKirarinAnimal
+    , "ライオン！"
+    ]
   , mkPurification
-      ["CureFriendy"]
-      ["FriendlyTact", "KirarinFawn"]
-      [ friendlyTact
-      , fureFureFriendy
-      , helpKirarinAnimal
-      , "コジカ！"
-      ]
+    ["CureFriendy"]
+    ["FriendlyTact", "KirarinFawn"]
+    [ friendlyTact
+    , fureFureFriendy
+    , helpKirarinAnimal
+    , "コジカ！"
+    ]
+  , mkPurification
+    ["CureFriendy"]
+    ["FriendlyTact", "KirarinBear"]
+    [ friendlyTact
+    , fureFureFriendy
+    , helpKirarinAnimal
+    , "ベアー！"
+    ]
+  , mkPurification
+    ["CureFriendy"]
+    ["FriendlyTact", "KirarinSwan"]
+    [ friendlyTact
+    , fureFureFriendy
+    , helpKirarinAnimal
+    , "スワン！"
+    ]
 
   , mkPurification
-      ["CureNyammy"]
-      ["AmityRibbonTambourine", "KirarinFox"]
-      [ amityRibbonTambourine
-      , nyanNyanNyammy
-      , helpKirarinAnimal
-      , "キツネ！"
-      ]
+    ["CureNyammy"]
+    ["AmityRibbonTambourine", "KirarinFox"]
+    [ amityRibbonTambourine
+    , nyanNyanNyammy
+    , helpKirarinAnimal
+    , "キツネ！"
+    ]
+  , mkPurification
+    ["CureNyammy"]
+    ["AmityRibbonTambourine", "KirarinPanda"]
+    [ amityRibbonTambourine
+    , nyanNyanNyammy
+    , helpKirarinAnimal
+    , "パンダ！"
+    ]
+  , mkPurification
+    ["CureNyammy"]
+    ["AmityRibbonTambourine", "KirarinFawn"]
+    [ amityRibbonTambourine
+    , nyanNyanNyammy
+    , helpKirarinAnimal
+    , "コジカ！"
+    ]
+  , mkPurification
+    ["CureNyammy"]
+    ["AmityRibbonTambourine", "KirarinHamster"]
+    [ amityRibbonTambourine
+    , nyanNyanNyammy
+    , helpKirarinAnimal
+    , "ハムスター！"
+    ]
+  , mkPurification
+    ["CureNyammy"]
+    ["AmityRibbonTambourine", "KirarinBear"]
+    [ amityRibbonTambourine
+    , nyanNyanNyammy
+    , helpKirarinAnimal
+    , "ベアー！"
+    ]
+  , mkPurification
+    ["CureNyammy"]
+    ["AmityRibbonTambourine", "KirarinSwan"]
+    [ amityRibbonTambourine
+    , nyanNyanNyammy
+    , helpKirarinAnimal
+    , "スワン！"
+    ]
+  , mkPurification
+    ["CureNyammy"]
+    ["AmityRibbonTambourine", "KirarinLion"]
+    [ amityRibbonTambourine
+    , nyanNyanNyammy
+    , helpKirarinAnimal
+    , "ライオン！"
+    ]
 
   , mkPurification
-      ["CureNyammy"]
-      ["AmityRibbonTambourine", "KirarinPanda"]
-      [ amityRibbonTambourine
-      , nyanNyanNyammy
-      , helpKirarinAnimal
-      , "パンダ！"
-      ]
+    ["CureLillian"]
+    ["AmityRibbonTambourine", "KirarinPenguin"]
+    [ amityRibbonTambourine
+    , rinrinLillian
+    , helpKirarinAnimal
+    , "ペンギン！"
+    ]
+  , mkPurification
+    ["CureLillian"]
+    ["AmityRibbonTambourine", "KirarinSwan"]
+    [ amityRibbonTambourine
+    , rinrinLillian
+    , helpKirarinAnimal
+    , "スワン！"
+    ]
+  , mkPurification
+    ["CureLillian"]
+    ["AmityRibbonTambourine", "KirarinBear"]
+    [ amityRibbonTambourine
+    , rinrinLillian
+    , helpKirarinAnimal
+    , "ベアー！"
+    ]
+  , mkPurification
+    ["CureLillian"]
+    ["AmityRibbonTambourine", "KirarinFawn"]
+    [ amityRibbonTambourine
+    , rinrinLillian
+    , helpKirarinAnimal
+    , "コジカ！"
+    ]
+  , mkPurification
+    ["CureLillian"]
+    ["AmityRibbonTambourine", "KirarinLion"]
+    [ amityRibbonTambourine
+    , rinrinLillian
+    , helpKirarinAnimal
+    , "ライオン！"
+    ]
+  , mkPurification
+    ["CureLillian"]
+    ["AmityRibbonTambourine", "KirarinFox"]
+    [ amityRibbonTambourine
+    , rinrinLillian
+    , helpKirarinAnimal
+    , "キツネ！"
+    ]
 
   , mkPurification
-      ["CureWonderful", "CureFriendy"]
-      [ "FriendlyTact"
-      , "FriendlyTact"
-      ]
-      [ friendlyTact
-      , "ワンダフルをきみに！"
-      , "ワン！ワン！わ～ん！"
-      , "ガルガルなこころ、とんでけ～！"
-      , "プリキュア！フレンドリベラーレ！"
-      ]
+    ["CureWonderful", "CureFriendy"]
+    [ "FriendlyTact", "FriendlyTact"]
+    [ friendlyTact
+    , "ワンダフルをきみに！"
+    , "ワン！ワン！わ～ん！"
+    , "ガルガルなこころ、とんでけ～！"
+    , "プリキュア！フレンドリベラーレ！"
+    ]
   , mkPurification
-      ["CureNyammy", "CureLillian"]
-      [ "AmityRibbonTambourine"
-      , "AmityRibbonTambourine"
-      ]
-      [ amityRibbonTambourine
-      , "ニャンダフルをあなたに！"
-      , "ニャン！ドゥ！トロワ！"
-      , "ガルガルなこころ、さようなら！"
-      , "プリキュア！アミティールミエール！"
-      ]
+    ["CureNyammy", "CureLillian"]
+    [ "AmityRibbonTambourine" , "AmityRibbonTambourine"]
+    [ amityRibbonTambourine
+    , "ニャンダフルをあなたに！"
+    , "ニャン！ドゥ！トロワ！"
+    , "ガルガルなこころ、さようなら！"
+    , "プリキュア！アミティールミエール！"
+    ]
+
+  , mkPurification
+    groupMembers_diamondRibbon
+    ["Niko", mkIA "DiamondRibbonCastle" ["DiaHeartKey"]]
+    ["も～っと友達！", "プリキュア！エターナルキズナシャワー！"]
   ]
  where
   friendlyTact :: String
@@ -279,6 +470,9 @@ purifications =
 
   nyanNyanNyammy :: String
   nyanNyanNyammy = "ニャン ニャン ニャミー！"
+
+  rinrinLillian :: String
+  rinrinLillian = "リンリン リリアン！"
 
 
 introducesHerselfAs_Wonderful :: String
